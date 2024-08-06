57cce65cf939ac90d2fde3b0ab3318089052f49b
yyhago <yhago.felipe.teles@gmail.com>
2024-08-06 19:20:50 -0300

    Trabalhando com promisse e exibindo HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,33 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+const pokemonListHtml = document.getElementsByClassName('pokemonsItens')[0];
 
-fetch(url).then(function(response){
-  console.log(response);
-})
+fetch(url)
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemonList) => {
+    pokemonListHtml.innerHTML = '';
+    pokemonList.forEach((pokemon) => {
+      pokemonListHtml.innerHTML += pokemonConvertToHtml(pokemon);
+    });
+  })
+  .catch((error) => console.log(error));
+
+function pokemonConvertToHtml(pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+
+      <div class="detail">
+        <ol class="types">
+          <li class="type">grass</li>
+          <li class="type">poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+      </div>
+    </li>
+  `;
+}
