13f14496e7155e7f6cfd95907b9084e977f4c640
yyhago <yhago.felipe.teles@gmail.com>
2024-08-06 19:49:03 -0300

    Separando API do consumo do HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,10 @@
 const offset = 0;
 const limit = 10;
 
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 const pokemonListHtml = document.getElementsByClassName('pokemonsItens')[0];
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemonList) => {
+
+pokeAPI.getPokemons(offset, limit).then((pokemonList) => {
     pokemonListHtml.innerHTML = '';
     pokemonList.forEach((pokemon) => {
       pokemonListHtml.innerHTML += pokemonConvertToHtml(pokemon);
