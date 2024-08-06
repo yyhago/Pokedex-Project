3c2869b89e709cb7b5e9aa08f89b0edbe417a625
yyhago <yhago.felipe.teles@gmail.com>
2024-08-06 19:52:29 -0300

    Código Comentado!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,37 @@
+// Define o offset inicial e o número de Pokémon a serem buscados
 const offset = 0;
 const limit = 10;
 
+// Seleciona o primeiro elemento com a classe 'pokemonsItens' para inserir a lista de Pokémon
 const pokemonListHtml = document.getElementsByClassName('pokemonsItens')[0];
 
-
+// Chama a função para buscar os Pokémon da API com o offset e limite definidos
 pokeAPI.getPokemons(offset, limit).then((pokemonList) => {
+    // Limpa o conteúdo HTML do elemento pokemonListHtml
     pokemonListHtml.innerHTML = '';
+    
+    // Itera sobre cada Pokémon na lista retornada e adiciona seu HTML ao elemento
     pokemonList.forEach((pokemon) => {
-      pokemonListHtml.innerHTML += pokemonConvertToHtml(pokemon);
+        // Converte o Pokémon para HTML e insere no elemento pokemonListHtml
+        pokemonListHtml.innerHTML += pokemonConvertToHtml(pokemon);
     });
-  })
-  .catch((error) => console.log(error));
+}).catch((error) => console.log(error)); // Exibe erros no console
 
+// Função que converte um objeto Pokémon para uma string HTML
 function pokemonConvertToHtml(pokemon) {
-  return `
-    <li class="pokemon">
-      <span class="number">#001</span>
-      <span class="name">${pokemon.name}</span>
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
 
-      <div class="detail">
-        <ol class="types">
-          <li class="type">grass</li>
-          <li class="type">poison</li>
-        </ol>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-      </div>
-    </li>
-  `;
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>
+        </li>
+    `;
 }
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,11 +1,14 @@
+// Define um objeto para armazenar funções relacionadas à API de Pokémon
 const pokeAPI = {};
 
+// Função que busca uma lista de Pokémon da API, com suporte a offset e limit
 pokeAPI.getPokemons = (offset = 0, limit = 10) => {
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    // Monta a URL para a requisição, usando os parâmetros offset e limit
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-  return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error));
+    // Faz uma requisição HTTP GET para a URL da API
+    return fetch(url)
+        .then((response) => response.json()) // Converte a resposta para JSON
+        .then((jsonBody) => jsonBody.results) // Retorna apenas a lista de resultados de Pokémon
+        .catch((error) => console.error(error)); // Exibe erros no console
 };
-
