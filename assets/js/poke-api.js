758c84c2ac36e7c801f06732fc1baac02f26e0a9
yyhago <yhago.felipe.teles@gmail.com>
2024-08-11 18:39:51 -0300

    Manipulando multiplas requisições e criando modal!

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,5 +1,29 @@
 const pokeAPI = {};
 
+function convertPokeAPIDetailsToPokemon(pokeDetails){
+    const pokemon = new Pokemon();
+
+    pokemon.number = pokeDetails.order;
+    pokemon.name = pokeDetails.name;
+
+    const types = pokeDetails.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types
+
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.photo = pokeDetails.sprites.other.dream_world.front_default;
+
+    return pokemon;
+}
+
+
+pokeAPI.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeAPIDetailsToPokemon)
+}
+
 pokeAPI.getPokemons = (offset = 0, limit = 10) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
@@ -7,5 +31,7 @@ pokeAPI.getPokemons = (offset = 0, limit = 10) => {
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetails))
+        .then((detailRequests) => Promise.all(detailRequests))
         .catch((error) => console.error(error));
 };
